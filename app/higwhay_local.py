167205d2d432eb5508cc1c98cdd73019b62096d6
GentiliEmanuele <egentili2000@gmail.com>
2024-06-06 17:56:21 +0200

    Remove some warnings

--- app/higwhay_local.py
+++ app/higwhay_local.py
@@ -2,9 +2,11 @@ import gymnasium as gym
 from video_utils import record_videos
 import warnings
 from tabulate import tabulate
+from matplotlib import pyplot as plt
 
 # Suppress the specific warning message
-warnings.filterwarnings("ignore", category=UserWarning, message=".*env.action_type to get variables from other wrappers is deprecated.*")
+warnings.filterwarnings("ignore", category=UserWarning, message=".*env.action_type to get variables from other "
+                                                                "wrappers is deprecated.*")
 # warnings.filterwarnings("ignore", category=UserWarning, message=".*env.configure.*")
 # warnings.filterwarnings("ignore", category=UserWarning, message=".*Overwriting existing videos.*")
 
@@ -14,18 +16,19 @@ config = {
         "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
         "absolute": True,
         "order": "sorted",
-        "vehicles_count": 5, # max number of observable vehicles
+        "vehicles_count": 5, #max number of observable vehicles
         "normalize": False
     },
     "action": {
         "type": "DiscreteMetaAction",
     },
     "lanes_count": 5,
-    "vehicles_count": 10, # max number of existing vehicles
+    "vehicles_count": 10, #max number of existing vehicles
     "duration": 40,  # [s]
     "initial_spacing": 2,
     "collision_reward": -1,  # The reward received when colliding with a vehicle.
-    "reward_speed_range": [20, 30],  # [m/s] The reward for high speed is mapped linearly from this range to [0, HighwayEnv.HIGH_SPEED_REWARD].
+    "reward_speed_range": [20, 30], # [m/s] The reward for high speed is mapped linearly from this range to [0,
+    # HighwayEnv.HIGH_SPEED_REWARD].
     "simulation_frequency": 15,  # [Hz]
     "policy_frequency": 1,  # [Hz]
     "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
@@ -48,10 +51,14 @@ env.configure(config)
 
 print("Gym configure")
 
-obs, info = env.reset(seed = 666)
+obs, info = env.reset(seed=666)
+
+# See initial configuration
+plt.imshow(env.render())
+plt.show()
 
 features = ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"]
-print(tabulate(obs, headers = features, tablefmt="grid"))
+print(tabulate(obs, headers=features, tablefmt="grid"))
 
 env = record_videos(env)
 
@@ -59,7 +66,7 @@ done = False
 truncated = False
 
 
-while (not done and not truncated):
+while not done and not truncated:
     action = env.action_type.actions_indexes["FASTER"]
     obs, reward, done, truncated, info = env.step(action)
 
